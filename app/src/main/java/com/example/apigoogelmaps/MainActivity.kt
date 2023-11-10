cbedf8747ad63560b58d995223998f0f9a874c95
OrozcoDev <juanjoseorozco9@gmail.com>
2023-11-10 18:22:44 -0500

    Initial commit

---
+++ app/src/main/java/com/example/apigoogelmaps/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.apigoogelmaps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdate
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var map:GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        createFragment()
+    }
+
+    private fun createFragment() {
+        val mapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        createMarker()
+    }
+
+    private fun createMarker() {
+        val coordinates = LatLng(2.442247, -76.602927)
+        val marker = MarkerOptions().position(coordinates).title("Sena, Centro de comercio y servicios")
+        map.addMarker(marker)
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordinates, 18f),
+            4000,null
+        )
+    }
+}
